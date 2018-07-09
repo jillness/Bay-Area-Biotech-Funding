a3e67e14be4ecbf3b9721a44e9b10c7aa3a064fd
Jillness <jillaseymour@mail.com>
2018-07-09 12:16:06 -0700

    app updated

--- app.py
+++ app.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def dashboard():
     return render_template('index.html') #"Render Dashboard!"
 
+@app.route("/")
+def presentation():
+    return render_template('presentation.html') #"Render Presentation page!"
+
+@app.route("/")
+def tables():
+    return render_template('tables.html') #"Render Tables page!"
+
 if __name__ == "__main__":
     app.run(debug=True)
